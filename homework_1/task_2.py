bb653a227e93111c9735bf1178a0f50ce3b69939
pnakongit <113530699+pnakongit@users.noreply.github.com>
2022-11-20 11:56:16 +0200

    Update task_2.py

--- homework_1/task_2.py
+++ homework_1/task_2.py
@@ -2,16 +2,14 @@ import psutil
 import functools
 
 
-def used_virtual_memory():
-    def internal(f):
-        @functools.wraps(f)
-        def virtual_memory_check(*args, **kwargs):
-            before_func = psutil.virtual_memory().available
-            result = f(*args, **kwargs)
-            after_func = psutil.virtual_memory().available
-            print(f'Було викрористано байт памяті: {before_func - after_func}')
-            return result
+def used_virtual_memory(f):
+    @functools.wraps(f)
+    def virtual_memory_check(*args, **kwargs):
+        before_func = psutil.virtual_memory().available
+        result = f(*args, **kwargs)
+        after_func = psutil.virtual_memory().available
+        used_memory = before_func - after_func
+        print (f'Функція "{f.__name__}" використала {psutil._common.bytes2human(used_memory)} памяті')
+        return result
 
-        return virtual_memory_check
-
-    return internal
+    return virtual_memory_check
